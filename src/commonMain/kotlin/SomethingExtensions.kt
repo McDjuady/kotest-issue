c0b79332f6f7153e30f67f3bf1323c270abcfffb
Maximilian Micko <mcdjuady@googlemail.com>
2022-12-14 11:03:56 +0100

    This is a workaround initializing the condition before the test run Tests are now running, however skipping a test at this stage causes mocha to report the test as failed instead of skipped

--- src/commonMain/kotlin/SomethingExtensions.kt
+++ src/commonMain/kotlin/SomethingExtensions.kt
@@ -1,31 +1,41 @@
 import io.kotest.core.extensions.SpecExtension
+import io.kotest.core.extensions.TestCaseExtension
 import io.kotest.core.spec.Spec
 import io.kotest.core.test.Enabled
 import io.kotest.core.test.EnabledOrReasonIf
+import io.kotest.core.test.TestCase
+import io.kotest.core.test.TestResult
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 
-object SomethingExtensions : SpecExtension {
+object SomethingExtensions : TestCaseExtension {
 
-    private var initialized = false
+    private lateinit var enabledInitialized: Enabled
+    private lateinit var disabledInitialized: Enabled
     val enabledOrReasonIf: EnabledOrReasonIf = {
-        if (initialized)
+        //On JS this is called once before the intercept on the testCase is called. Skip it in that case
+        if (!this::enabledInitialized.isInitialized)
             Enabled.enabled
         else
-            Enabled.disabled("Not initialized")
+            enabledInitialized
     }
 
     val oppositeEnabledOrReasonIf: EnabledOrReasonIf = {
-        if (!initialized)
+        //On JS this is called once before the intercept on the testCase is called. Skip it in that case
+        if (!this::disabledInitialized.isInitialized)
             Enabled.enabled
         else
-            Enabled.disabled("Not initialized")
+            disabledInitialized
     }
 
-    override suspend fun intercept(spec: Spec, execute: suspend (Spec) -> Unit) {
-        initialized = true
-        runPromise()
-        execute(spec)
+    override suspend fun intercept(testCase: TestCase, execute: suspend (TestCase) -> TestResult): TestResult {
+        //Ensure our condition is initialized. EnabledCheckInterceptor will run later to confirm via EnabledOrReasonIf
+        if (!this::enabledInitialized.isInitialized) {
+            runPromise()
+            enabledInitialized = Enabled.enabled
+            disabledInitialized = Enabled.disabled("Disabled")
+        }
+        return execute(testCase)
     }
 
 }
